10a7a5ec761763920d6594278f8a450fd617c3c1
Richard Zvonek <zvonek.r@seznam.cz>
2020-11-12 20:43:17 +0100

    Basic opencv viz app

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#include <opencv2/viz/widgets.hpp>
+#include <opencv2/viz/viz3d.hpp>
+
 int main(int argc, const char **argv) {
-  std::cout << "Hello world" << std::endl;
+  cv::viz::Viz3d window = cv::viz::Viz3d("Marching cubes");
+  
+  cv::viz::WCube mainCube(cv::Point3f(0.5,0.5,0.5), cv::Point3f(1,1,1), true, cv::viz::Color::blue());
+  mainCube.setRenderingProperty(cv::viz::LINE_WIDTH, 4.0);
+  window.showWidget("Coordinate Widget", cv::viz::WCoordinateSystem());
+  window.showWidget("Cube widget", mainCube);
+  
+  window.spin();
   return 0;
 }
