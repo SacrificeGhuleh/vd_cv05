a7d193852cfccdd681e4c4df948284d13d6cdf2e
Richard Zvonek <zvonek.r@seznam.cz>
2020-11-12 21:51:34 +0100

    Visualization of vertices for marching

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 #include <opencv2/viz/widgets.hpp>
 #include <opencv2/viz/viz3d.hpp>
+#include <opencv2/flann/miniflann.hpp>
+#include <opencv2/viz/vizcore.hpp>
 
 #include "particle.h"
 #include "cube.h"
@@ -10,8 +12,9 @@
 //const float step = 0.0125f;
 const float step = 0.1f;
 //const float step = 0.2f;
-
 const float halfStep = (step / 2.f);
+const float threshold = 600;
+const float maxDist = sqrt((step * step) + (step / 2.f) * (step / 2.f));
 
 void loadData(std::vector<Particle> &particles, const std::string &filename) {
   std::ifstream file;
@@ -43,12 +46,27 @@ void loadData(std::vector<Particle> &particles, const std::string &filename) {
   file.close();
 }
 
+template<class T>
+T gaussianKernel(const T &r, const T &h) {
+  T ret = std::cbrt(M_PI) * (h * h * h);
+  ret = 1.f / ret;
+  ret *= std::exp(-((r / h) * (r / h)));
+  return ret;
+}
+
 int main(int argc, const char **argv) {
   const cv::Point3d min(-0.5f, -0.5f, 0.f);
   const cv::Point3d max(0.5f, 0.5f, 1.f);
   
+  cv::Vec3f cam_pos(2.0f,2.0f,2.0f);
+  cv::Vec3f cam_focal_point(0.0f,0.0f,0.5f);
+  cv::Vec3f cam_y_dir(0.0f,0.0f,-1.0f);
+  cv::Affine3f cam_pose = cv::viz::makeCameraPose(cam_pos, cam_focal_point, cam_y_dir);
+  
   
   cv::viz::Viz3d window = cv::viz::Viz3d("Marching cubes");
+  window.setWindowSize(cv::Size_(500,500));
+  window.setViewerPose(cam_pose);
   
   cv::viz::WCube mainCube(min, max, true, cv::viz::Color::blue());
   mainCube.setRenderingProperty(cv::viz::LINE_WIDTH, 4.0);
@@ -59,22 +77,31 @@ int main(int argc, const char **argv) {
   loadData(particles, "./data/sph_001100.bin");
   
   cv::Mat3f pointCloud(1, 64000);
-//  cv::Mat3b pointCloudColors(1, 64000);
   
+  auto samplePoints = cv::Mat1f(64000, 3);
   int id = 0;
   for (const Particle &particle : particles) {
     pointCloud.at<cv::Vec3f>(id) = cv::Vec3f(particle.position_x, particle.position_y, particle.position_z);
-//    pointCloudColors.at<cv::Vec3b>(id) = colors[id%colorsSize];
+
+    samplePoints.at<float>(id, 0) = particle.position_x;
+    samplePoints.at<float>(id, 1) = particle.position_y;
+    samplePoints.at<float>(id, 2) = particle.position_z;
     
     id++;
   }
+
+  cv::flann::Index flannIndex = cv::flann::Index(samplePoints, cv::flann::KDTreeIndexParams(), cvflann::FLANN_DIST_L2);
+  
   cv::viz::WCloud myCloud(pointCloud);
   window.showWidget("My cloud", myCloud);
+  
+  int sizes[] = {static_cast<int>(1.f/step)+1,static_cast<int>(1.f/step)+1,static_cast<int>(1.f/step)+1};
+  cv::Mat1f edgesValues(3, sizes);
+  cv::Mat_<bool> alreadyInitialized(3, sizes, false);
   
   while (!window.wasStopped()) {
     cv::Point3d marchingMax = min + cv::Point3d(step, step, step);
     id = 0;
-    
     for (int x = 0; x < 1. / step; x++) {
       marchingMax.y = min.y + step;
       for (int y = 0; y < 1. / step; y++) {
@@ -91,7 +118,36 @@ int main(int argc, const char **argv) {
           window.showWidget(ss.str(), marchingCube);
           window.showWidget("My cloud", myCloud);
           
-          window.spinOnce(1, true);
+          std::vector<int> indices;
+          std::vector<float> distances;
+
+          for (int j = 0; j < 8; j++) {
+            const cv::Point3f &pointInCube = cube.points.at(j);
+            std::vector<float> query = {pointInCube.x, pointInCube.y, pointInCube.z};
+            float rho = 0;
+            int n = flannIndex.radiusSearch(query, indices, distances, maxDist, 10 * 1024, cv::flann::SearchParams(256));
+            if (n > 0) {
+              const int indicesCount = std::min<int>(indices.size(), n);
+              for (int i = 0; i < indicesCount; i++) {
+                const float distance = distances.at(i);
+                const int indice = indices.at(i);
+                const Particle &particle = particles.at(indice);
+                rho += particle.rho * gaussianKernel(distance, maxDist);
+              }
+            }
+            cube.values[j] = rho;
+            if (rho > threshold) {
+              std::stringstream ssPt;
+              ssPt << "Point " << j;
+
+              window.showWidget(ssPt.str(), cv::viz::WSphere(pointInCube, step / 20.f));
+            }
+            j++;
+          }
+          
+//          window.resetCamera();
+          auto cam  = window.getCamera();
+          window.spinOnce(10, true);
           window.removeAllWidgets();
           
           id++;
